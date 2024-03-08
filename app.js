0c2ef4056c71993f95458e7068e144826c4bae0a
AmandeepKaur <140660534+Amandeepdeolgill@users.noreply.github.com>
2024-03-08 14:22:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ let btn = document.querySelector("button");
 let inp = document.querySelector("input");
 let ul = document.querySelector("ul");
 
+function getRandomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
+
 btn.addEventListener("click", ()=> {
     let list = document.createElement("li");
     list.innerText =  inp.value;
     ul.appendChild(list);
     inp.value = ""; 
+    list.style.backgroundColor = getRandomColor();
+    list.style.fontWeight = "bold";
     let delbtn = document.createElement("button");
     delbtn.innerText = "Remove";
     delbtn.classList.add("del");
     list.appendChild(delbtn);
 });
 ul.addEventListener("click", (event) => {
-    console.dir(event.target);
     if (event.target.classList.contains("del")) {
         let liItem = event.target.parentElement;
         liItem.remove();
